c35e97dca37a239bf5b1b6a8b03d0430f05c6a76
Marcus Pará <marcus.para@elo7.com>
2022-10-11 09:15:23 -0300

    fix: vendas por cliente

--- src/main/java/br/com/alura/clientelo/ClienteEstatisticas.java
+++ src/main/java/br/com/alura/clientelo/ClienteEstatisticas.java
@@ -1,6 +1,8 @@
 package br.com.alura.clientelo;
 
-public class ClienteEstatisticas {
+import java.util.Objects;
+
+public class ClienteEstatisticas implements Comparable<ClienteEstatisticas> {
     private String nome;
     private Integer qtdPedidos;
 
@@ -23,5 +25,33 @@ public class ClienteEstatisticas {
 
     public void adicionaPedido(Pedido pedido) {
         this.qtdPedidos += pedido.getQuantidade();
+    }
+
+    @Override
+    public int compareTo(ClienteEstatisticas clienteEstatisticas) {
+        if (clienteEstatisticas != null) {
+//            if (!this.qtdPedidos.equals(clienteEstatisticas.qtdPedidos))
+//                return -this.qtdPedidos.compareTo(clienteEstatisticas.qtdPedidos);
+//            return this.nome.compareTo(clienteEstatisticas.nome);
+
+            if (this.qtdPedidos.equals(clienteEstatisticas.qtdPedidos)) return this.nome.compareTo(clienteEstatisticas.nome);
+            return -this.qtdPedidos.compareTo(clienteEstatisticas.qtdPedidos);
+
+        }
+
+        throw new NullPointerException();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ClienteEstatisticas that = (ClienteEstatisticas) o;
+        return Objects.equals(nome, that.nome) && Objects.equals(qtdPedidos, that.qtdPedidos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, qtdPedidos);
     }
 }
--- src/main/java/br/com/alura/clientelo/PedidosEstatisticas.java
+++ src/main/java/br/com/alura/clientelo/PedidosEstatisticas.java
@@ -3,19 +3,19 @@ package br.com.alura.clientelo;
 import java.util.*;
 
 public class PedidosEstatisticas {
-    private final Set<ProdutoEstatisticas> produtosEstatisticas;
-    private final Map<String, ProdutoEstatisticas> produtos;
+    private final Set<ProdutoEstatisticas> produtos;
+    private final Map<String, ProdutoEstatisticas> produtoToEstatisticas;
     private final Set<String> categorias;
-    private final Map<String, CategoriaEstatisticas> categoriasEstatisticas;
-    private final Set<String> clientes;
-    private final Map<String, ClienteEstatisticas> clientesEstatisticas;
+    private final Map<String, CategoriaEstatisticas> categoriaToEstatisticas;
+    private final Set<ClienteEstatisticas> clientes;
+    private final Map<String, ClienteEstatisticas> clienteToEstatisticas;
 
     public PedidosEstatisticas() {
-        this.produtosEstatisticas = new TreeSet<>();
-        this.produtos = new HashMap<>();
-        this.categoriasEstatisticas = new HashMap<>();
+        this.produtos = new TreeSet<>();
+        this.produtoToEstatisticas = new HashMap<>();
+        this.categoriaToEstatisticas = new HashMap<>();
         this.categorias = new TreeSet<>();
-        this.clientesEstatisticas = new HashMap<>();
+        this.clienteToEstatisticas = new HashMap<>();
         this.clientes = new TreeSet<>();
     }
 
@@ -34,7 +34,7 @@ public class PedidosEstatisticas {
     }
 
     private void insertCliente(Pedido pedido, String cliente) {
-        if (!clientes.contains(cliente)) {
+        if (!clienteToEstatisticas.containsKey(cliente)) {
             insertNovoCliente(pedido, cliente);
         }
         else {
@@ -43,14 +43,16 @@ public class PedidosEstatisticas {
     }
 
     private void updateCliente(Pedido pedido, String cliente) {
-        ClienteEstatisticas clienteEstatisticas = clientesEstatisticas.get(cliente);
+        ClienteEstatisticas clienteEstatisticas = clienteToEstatisticas.get(cliente);
+        clientes.remove(clienteEstatisticas);
         clienteEstatisticas.adicionaPedido(pedido);
+        clientes.add(clienteEstatisticas);
     }
 
     private void insertNovoCliente(Pedido pedido, String cliente) {
         ClienteEstatisticas clienteEstatisticas = new ClienteEstatisticas(pedido);
-        clientes.add(cliente);
-        clientesEstatisticas.put(cliente, clienteEstatisticas);
+        clientes.add(clienteEstatisticas);
+        clienteToEstatisticas.put(cliente, clienteEstatisticas);
     }
 
     private void insertCategoria(Pedido pedido, String categoria) {
@@ -63,36 +65,36 @@ public class PedidosEstatisticas {
     }
 
     private void updateCategoria(Pedido pedido, String categoria) {
-        CategoriaEstatisticas categoriaEstatisticas = categoriasEstatisticas.get(categoria);
+        CategoriaEstatisticas categoriaEstatisticas = categoriaToEstatisticas.get(categoria);
         categoriaEstatisticas.adicionaPedido(pedido);
     }
 
     private void insertNovaCategoria(Pedido pedido, String categoria) {
         CategoriaEstatisticas categoriaEstatisticas = new CategoriaEstatisticas(pedido);
         categorias.add(categoria);
-        categoriasEstatisticas.put(categoria, categoriaEstatisticas);
+        categoriaToEstatisticas.put(categoria, categoriaEstatisticas);
     }
 
     private void insertProduto(Pedido pedido, String produto) {
-        if (!produtos.containsKey(produto)) insertNovoProduto(pedido, produto);
+        if (!produtoToEstatisticas.containsKey(produto)) insertNovoProduto(pedido, produto);
         else updateProduto(pedido, produto);
     }
 
     private void updateProduto(Pedido pedido, String produto) {
-        ProdutoEstatisticas produtoEstatisticas = produtos.get(produto);
+        ProdutoEstatisticas produtoEstatisticas = produtoToEstatisticas.get(produto);
         produtoEstatisticas.adicionaNVendas(pedido.getQuantidade());
     }
 
     private void insertNovoProduto(Pedido pedido, String produto) {
         ProdutoEstatisticas produtoEstatisticas = new ProdutoEstatisticas(pedido);
-        produtosEstatisticas.add(produtoEstatisticas);
-        produtos.put(produto, produtoEstatisticas);
+        produtos.add(produtoEstatisticas);
+        produtoToEstatisticas.put(produto, produtoEstatisticas);
     }
 
     public List<ProdutoEstatisticas> produtosMaisVendidos(int n) {
         List<ProdutoEstatisticas> maisVendidos = new ArrayList<>(n);
 
-        Iterator<ProdutoEstatisticas> iterator = produtosEstatisticas.iterator();
+        Iterator<ProdutoEstatisticas> iterator = produtos.iterator();
         int i = 0;
         while (iterator.hasNext() && i < 3) {
             maisVendidos.add(iterator.next());
@@ -106,15 +108,15 @@ public class PedidosEstatisticas {
         List<CategoriaEstatisticas> answer = new ArrayList<>();
         for (String categoria :
                 categorias) {
-            answer.add(categoriasEstatisticas.get(categoria));
+            answer.add(categoriaToEstatisticas.get(categoria));
         }
         return answer;
     }
 
     public List<ClienteEstatisticas> vendasPorCliente() {
         List<ClienteEstatisticas> answer = new ArrayList<>();
-        for (String cliente: clientes) {
-            answer.add(clientesEstatisticas.get(cliente));
+        for (ClienteEstatisticas cliente: clientes) {
+            answer.add(clienteToEstatisticas.get(cliente.getNome()));
         }
         return answer;
     }
